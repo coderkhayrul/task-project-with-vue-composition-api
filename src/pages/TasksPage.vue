1981cb90471c8a26797c8cdc3aa560b0004e440c
coderkhayrul <coderkhayrul@gmail.com>
2025-03-09 03:22:14 +0600

    Create Task Feature Added

---
+++ src/pages/TasksPage.vue
@@ -0,0 +1,65 @@
+<template>
+  <main style="min-height: 50vh; margin-top: 2rem">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <!-- Add new Task -->
+          <NewTask @added="handelAddedTask" />
+          <!-- List of uncompleted tasks -->
+          <Tasks :tasks="uncompletedTasks" />
+          <!-- show toggle button -->
+          <div class="text-center my-3" v-show="showToggleCompletedBtn">
+            <button
+              class="btn btn-sm btn-secondary"
+              @click="($event) => (showCompletedTasks = !showCompletedTasks)"
+            >
+              <span v-if="!showCompletedTasks">Show Completed</span>
+              <span v-else>Hide Completed</span>
+            </button>
+          </div>
+          <!-- List of completed tasks -->
+          <Tasks
+            :tasks="completedTasks"
+            :show="completeTaskIsVisible && showCompletedTasks"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { onMounted, ref, computed } from "vue";
+import { allTasks, createTask } from "../http/task-api.js";
+import Tasks from "../components/tasks/Tasks.vue";
+import NewTask from "../components/tasks/NewTask.vue";
+
+const tasks = ref([]);
+
+onMounted(async () => {
+  const { data } = await allTasks();
+  tasks.value = data.data;
+});
+
+const uncompletedTasks = computed(() =>
+  tasks.value.filter((task) => !task.is_completed)
+);
+const completedTasks = computed(() =>
+  tasks.value.filter((task) => task.is_completed)
+);
+
+const showToggleCompletedBtn = computed(
+  () => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0
+);
+
+const completeTaskIsVisible = computed(
+  () => uncompletedTasks.value.length > 0 || completedTasks.value.length > 0
+);
+
+const showCompletedTasks = ref(false);
+
+const handelAddedTask = async (NewTask) => {
+  const { data: createdTask } = await createTask(NewTask);
+  tasks.value.unshift(createdTask.data);
+};
+</script>
